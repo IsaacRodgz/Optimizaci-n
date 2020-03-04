FIX Gradient function

tarea4/gradient_descent.py
@@ -13,7 +13,7 @@ class GD:
         a list with all points x traversed during gradient descent
     """
 
-    def iterate(self, x0, mxitr, tol_g, tol_x, tol_f, f, msg, *args):
+    def iterate(self, x0, mxitr, tol_g, tol_x, tol_f, f, msg, function, *args):
         """Iterate over x_{k+1} = x_k - alpha_k * d_k
         Where d_k = gradient(f(x_k))
 
@@ -76,38 +76,44 @@ class GD:
             x_old = x
             x = x - alpha * grad
 
-            # Calculate different tolerance criteria
-            tol_x_val = np.linalg.norm(x - x_old) / max(1.0, np.linalg.norm(x_old))
-            tol_f_val = np.absolute(f.eval(x) - f.eval(x_old)) / max(1.0, np.absolute(f.eval(x_old)))
-            tol_g_val = np.linalg.norm(x_old)
-            tol_mnist = f.error(x)
+            if function == "mnist":
+                error = f.error(x)
+                loss = f.eval(x)
 
-            if k%1 == 0:
-                self.log2(x_old, grad, x, k, tol_g_val, tol_mnist, f.eval(x))
+                self.log(k, error, loss)
 
-            #log(x_old, grad, x, k, tol_g_val, np.linalg.norm(x - x_old), f(x))
+                if error < tol_f:
+                    print("\n Algorithm converged in error\n")
+                    break
+
+            else:
+                # Calculate different tolerance criteria
+                tol_x_val = np.linalg.norm(x - x_old) / max(1.0, np.linalg.norm(x_old))
+                tol_f_val = np.absolute(f.eval(x) - f.eval(x_old)) / max(1.0, np.absolute(f.eval(x_old)))
+                tol_g_val = np.linalg.norm(x_old)
+
+                if k%1 == 0:
+                    self.log2(x_old, grad, x, k, tol_g_val, tol_x_val, f.eval(x))
+
+                # Check for convergence
+                if tol_x_val < tol_x:
+                    print("\n Algorithm converged in x\n")
+                    break
+
+                if tol_f_val < tol_f:
+                    print("\n Algorithm converged in f\n")
+                    break
+
+                if tol_g_val < tol_g:
+                    print("\n Algorithm converged in g\n")
+                    break
+
+                if k > mxitr:
+                    print("\n Algorithm reached max num of iterations\n")
+                    break
 
             # Update iteration counter
             k += 1
-
-            # Check for convergence
-            if tol_x_val < tol_x:
-                print("\n Algorithm converged in x\n")
-                break
-
-            if tol_f_val < tol_f:
-                print("\n Algorithm converged in f\n")
-                break
-
-            if tol_g_val < tol_g:
-                print("\n Algorithm converged in g\n")
-                break
-
-            if k > mxitr:
-                print("\n Algorithm reached max num of iterations\n")
-                break
-
-        #self.log2(x_old, grad, x, k, tol_g_val, np.linalg.norm(x - x_old), f.eval(x))
 
         return xs
 
@@ -132,27 +138,27 @@ class GD:
 
         c_1 = 1e-4
 
-        # Check if alpha_init satisfies armijo conditions
-
-        if f.eval(x+alpha_init*d) <= f.eval(x) + c_1*alpha_init*(f.gradient(x).dot(d)):
-            return alpha_init
-
         alpha_0 = alpha_init
 
-        phi_p0 = f.gradient(x).dot(d)
+        phi_p0 = -d.dot(d)
         phi_0 = f.eval(x)
         phi_alpha_0 = f.eval(x+alpha_0*d)
+
+        # Check if alpha_init satisfies armijo conditions
+
+        if phi_alpha_0 <= phi_0 + c_1*alpha_0*(phi_p0):
+            return alpha_0
 
         alpha_1 = (-(alpha_0**2)*phi_p0) / (2*(phi_alpha_0 - phi_p0*alpha_0 - phi_0))
 
         # Check if alpha_1, obtained by cuadratic interpolation, satisfies armijo conditions
 
-        if f.eval(x+alpha_1*d) > f.eval(x) + c_1*alpha_1*(f.gradient(x).dot(d)):
+        phi_alpha_1 = f.eval(x+alpha_1*d)
+
+        if phi_alpha_1 > phi_0 + c_1*alpha_1*(phi_p0):
             return alpha_1
 
         # Perform cubic interpolation
-
-        phi_alpha_1 = f.eval(x+alpha_1*d)
 
         constant = 1/(alpha_0**2*alpha_1**2*(alpha_1-alpha_0))
         m1 = np.array([[alpha_0**2, -alpha_1**2], [-alpha_0**3, alpha_1**3]])
@@ -163,7 +169,7 @@ class GD:
 
         alpha_2 = (-b + np.sqrt(b**2 - 3*a*c)) / (3*a)
 
-        while f.eval(x+alpha_2*d) > f.eval(x) + c_1*alpha_2*(f.gradient(x).dot(d)):
+        while f.eval(x+alpha_2*d) > phi_0 + c_1*alpha_2*(phi_p0):
             alpha_0 = alpha_1
             alpha_1 = alpha_2
 
@@ -201,19 +207,15 @@ class GD:
         c_1 = 1e-4
         alpha_0 = alpha_init
 
-        phi_p0 = f.gradient(x).dot(d)
+        phi_p0 = -d.dot(d)
         phi_0 = f.eval(x)
         phi_alpha_0 = f.eval(x+alpha_0*d)
 
         alpha_1 = (-(alpha_0**2)*phi_p0) / (2*(phi_alpha_0 - phi_p0*alpha_0 - phi_0))
 
-        while f.eval(x+alpha_1*d) > c + c_1*alpha_1*(f.gradient(x).dot(d)):
+        while f.eval(x+alpha_1*d) > c + c_1*alpha_1*(phi_p0):
             alpha_0 = alpha_1
-
-            phi_p0 = f.gradient(x).dot(d)
-            phi_0 = f.eval(x)
             phi_alpha_0 = f.eval(x+alpha_0*d)
-
             alpha_1 = (-(alpha_0**2)*phi_p0) / (2*(phi_alpha_0 - phi_p0*alpha_0 - phi_0))
 
         return alpha_1
@@ -292,28 +294,13 @@ class GD:
 
         return xs
 
-    def log(self, x_old, grad, x, curr_iter, tol_g_val, tol_x_val, tol_f_val):
-        """Print to console status of current iteration
-
-        Args:
-            x_old (numpy array): Previous solution point
-            grad (numpy array): Gradient of the function at x_old
-            x (numpy array): Solution point after gradient step
-            curr_iter (int): Current number of iteration
-            tol_g (float): Tolerance for gradient norm
-            tol_x (float): Tolerance for x's relative error
-            tol_f (float): Tolerance for relative error in evaluation of function f
-
-        Output: Print to console status of gradient descent
+    def log(self, curr_iter, error, loss):
+        """
         """
         print("-----------------------------------")
         print("\n Iter: ", curr_iter)
-        print("\n x_old: ", x_old)
-        print("\n gradient: ", grad)
-        print("\n x: ", x)
-        print("\n tol_x_val: ", tol_x_val)
-        print("\n tol_f_val: ", tol_f_val)
-        print("\n tol_g_val: %s \n " % tol_g_val)
+        print("\n error: %s" % error)
+        print("\n loss: %s" % loss)
 
     def log2(self, x_old, grad, x, curr_iter, tol_g_val, tol_x_val, tol_f_val):
         """Print to console status of current iteration

tarea4/mnist.py
@@ -40,6 +40,7 @@ class MNIST:
         self.train_data  = np.c_[self.train_data, p]
 
         self.n = self.train_data.shape[1]
+        self.data_size = self.train_data.shape[0]
 
     def get_size(self):
         """Gets size of flattened image
@@ -62,12 +63,19 @@ class MNIST:
 
         sum = 0
 
-        for i in range(self.n):
+        for i in range(self.data_size):
 
             yi = self.train_labels[i]
             pi = self.sigmoid(self.train_data[i], beta)
 
-            sum += yi*np.log10(pi) + (1-yi)*np.log10(1-pi)
+            if yi == 1:
+                if pi == 0:
+                    pi = 1e-15
+                sum += yi*np.log10(pi)
+            else:
+                if pi == 1:
+                    pi = 1-1e-15
+                sum += (1-yi)*np.log10(1-pi)
 
         return sum
 
@@ -80,12 +88,13 @@ class MNIST:
             Weights of logistic regression
         """
 
-        grad = []
+        grad = np.zeros(self.n)
 
         for i in range(self.n):
-            grad.append(self.train_labels[i] - self.sigmoid(self.train_data[i], beta))
+            for j in range(self.data_size):
+                grad[i] += (self.train_labels[j] - self.sigmoid(self.train_data[j], beta))*self.train_data[j][i]
 
-        return np.array(grad)
+        return grad
 
     def error(self, beta):
         """Evaluates classification error on all the dataset

tarea4/test_function.py
@@ -30,11 +30,11 @@ def run(step, point_type, method, function):
             x0[0] = -1.2
             x0[-2] = -1.2
         elif function == "mnist":
-            x0 = 0.001*np.ones(n)
+            x0 = np.ones(n)
         else:
             x0 = np.array([-3, -1, -3, -1])
     elif point_type == "rand":
-        x0 = np.random.uniform(-2, 2, n)
+        x0 = np.random.uniform(-1, 1, n)
     else:
         print("\n Error, invalid type of point")
         quit()
@@ -52,8 +52,6 @@ def run(step, point_type, method, function):
         print("\n Error, invalid step method")
         quit()
 
-    step_size = 1  # Gradient step size for "StepFijo" method
-
     # Estimate minimum point through optimization method chosen
     if method == "gd":
         alg = GD()
@@ -63,7 +61,7 @@ def run(step, point_type, method, function):
         print("\n Error: Invalid optimization method: %s\n" % method)
         quit()
 
-    xs = alg.iterate(x0, mxitr, tol_g, tol_x, tol_f, f, step, step_size)
+    xs = alg.iterate(x0, mxitr, tol_g, tol_x, tol_f, f, step, function)
 
     # Print point x found and function value f(x)
     # print("\nPoint x found: ", xs[-1])